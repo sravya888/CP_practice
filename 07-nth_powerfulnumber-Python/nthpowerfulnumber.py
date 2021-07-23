965772afd73689ab1c4d433ddabd8a31b30b4342
sravya888 <72121831+sravya888@users.noreply.github.com>
2021-07-23 15:37:12 +0530

    c1

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -4,7 +4,30 @@
 # A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
+import math
+
+def isPowerful(n):
+    while (n % 2 == 0):
+        a = 0
+        while (n % 2 == 0):
+            n = n // 2
+            a = a + 1
+        if ( a == 1):
+            return False
+    for power in range(3, int(math.sqrt(n))+1, 2):
+        a = 0
+        while (n % power == 0):
+            n = n // power
+            a = a + 1
+        if (a == 1):
+            return False
+    return (n == 1)
 
 def nthpowerfulnumber(n):
-	# Your code goes here
-	pass
+    found = 0
+    guess = 0
+    while (found <= abs(n)):
+        guess += 1
+        if(isPowerful(guess)):
+            found += 1
+    return guess
