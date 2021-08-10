6ccd5753b9ead7587e46a5da0d1a89cdbceafcac
sravya888 <72121831+sravya888@users.noreply.github.com>
2021-08-10 09:18:41 +0530

    c1

--- topScorer.py
+++ topScorer.py
@@ -15,8 +15,24 @@
 # string "None"). So, for example:
 
 def topScorer(data):
-    # Your code goes here...
-    return ""
+    Fred=[]
+    Wilma=[]
+    if(data==''):
+        return None
+    x=data.splitlines()
+    for i in x:
+        s=i.split(",")
+        for j in range(1,len(s)):
+            if(s[0]=="Fred"):
+                Fred.append(int(s[j]))
+            else:
+                Wilma.append(int(s[j]))
+    if(max(Fred)>max(Wilma)):
+        return "Fred"
+    elif(max(Fred)==max(Wilma)):
+        return "Fred,Wilma"
+    else:
+        return "Wilma"
 
 data = '''\
 Fred,10,20,30,40
